64383c93f0897f50fa8a5a2d73ceb774b64deb58
Bruno Galvao <bruno_galvao@outlook.com>
2021-04-13 23:40:44 -0300

    meu primeiro commit

---
+++ js/controle.js
@@ -0,0 +1,14 @@
+function exe() {
+
+    var id = document.querySelector(".texto");
+    var text = "Agora simm";
+    
+    id.innerHTML = text;
+
+}
+
+function print () {
+    exe();
+}
+
+print();
